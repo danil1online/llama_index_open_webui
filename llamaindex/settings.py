54e2161b6927cbdefd61fa0257c611b75d13babc
danil1online <49284702+danil1online@users.noreply.github.com>
2026-03-13 19:23:14 +0300

    Add files via upload

---
+++ llamaindex/settings.py
@@ -0,0 +1,31 @@
+import os
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    mode: str = os.getenv("MODE", "hf")  # hf | ollama
+
+#    ollama_url: str = os.getenv("OLLAMA_URL", "http://ollama:11434")
+    qdrant_url: str = os.getenv("QDRANT_URL", "http://qdrant:6333")
+
+    collection_pdf: str = os.getenv("COLLECTION_PDF", "pdf_docs")
+    collection_code: str = os.getenv("COLLECTION_CODE", "code_docs")
+
+    # Настройки для LM
+    # host.docker.internal позволит контейнеру достучаться до lms на хосте
+    
+    # Если выбрали вариант LM Studio
+    #lms_url: str = os.getenv("LMS_URL", "http://host.docker.internal:1234/v1")
+    #llm_model: str = os.getenv("LLM_MODEL", "qwen3.5-4b")
+    
+    # Если выбрали вариант Ollama-docker
+    lms_url: str = os.getenv("LMS_URL", "http://host.docker.internal:11434/v1")
+    llm_model: str = os.getenv("LLM_MODEL", "qwen3.5:4b")
+
+    embed_model_hf: str = os.getenv("EMBED_MODEL_HF", "intfloat/multilingual-e5-large")
+
+    reranker_model: str = os.getenv("RERANKER_MODEL", "BAAI/bge-reranker-v2-m3")
+
+
+SETTINGS = Settings()
+MODE = SETTINGS.mode
